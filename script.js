5874ab9e00c82b34e35eb274a46229e021d30626
KevinGayda <kevingayda@gmail.com>
2025-07-24 14:37:37 -0700

    added more content

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const form = document.querySelector(".form");
+const form = document.querySelector(".form");//selecting the html element form with class .form
 const firstName = form.querySelector(".first_name_of_person");//remember to have a class in the input!!!
 
 form.addEventListener("submit", function(evt){
-    evt.preventDefault();
+    evt.preventDefault();//this line of code prevents the page from reloading when the form is submitted
     console.log(firstName.value);//prints the first name of the person to the consol and .value acces the value of the name
     form.reset();//resets the input field so the next person who wants to enter their first name has a clean field
 });
